457890b440500899833ee1e0c8bcb08ad6cca07f
RegisEmiel <regisemiel@yandex.ru>
2022-01-27 07:15:12 +0300

    Init commit

---
+++ src/test/java/com/geekbrains/market/MarketApplicationTests.java
@@ -0,0 +1,25 @@
+//package com.geekbrains.market;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MarketApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
+
+package com.geekbrains.market;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MarketApplicationTests {
+	public static void main(String[] args) {
+
+	}
+
+
+}
